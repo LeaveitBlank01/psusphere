c2d1e5858444822a2240d0ac8468e30a3c3c2321
leaveitBlank01 <202280102@psu.palawan.edu.ph>
2025-04-10 13:04:57 +0800

    fixed logout

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -128,3 +128,8 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication Settings
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -7,7 +7,10 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import College, Organization, Student, Program, OrgMember
 from studentorg.forms import CollegeForm, OrganizationForm, StudentForm, ProgramForm, OrgMemberForm
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
+@method_decorator(login_required, name='dispatch')
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
